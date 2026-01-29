e44675e859cf276c45456a41ab00dee21bed7b4a
Vani Kamra <vani@mac.lan>
2026-01-28 22:34:32 -0500

    navbar updates

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,22 +1,26 @@
 import { useState } from 'react'
 import './App.css'
 import Footer from './Footer'
+import NavBar from './NavBar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <h1>Welcome to the OTC Medical Supplies App!</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-        </div>
-        <p className="read-the-docs">
-          Under Construction
-        </p>
-        <Footer/>
+      <NavBar />
+
+      <h1>Welcome to the OTC Medical Supplies App!</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Under Construction
+      </p>
+
+      <Footer/>
     </>
   )
 }
